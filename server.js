643054e8ab2263ea8588c9bd8227a99f9b8cb14c
Mr. Software <weahjrdanielk@gmail.com>
2025-02-26 00:21:51 +0000

    Updated with redis online connection

--- server.js
+++ server.js
@@ -47,7 +47,21 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 const Redis = require("ioredis");
-const redis = new Redis();
+
+const redis = new Redis({
+  host: process.env.REDIS_HOST,  
+  port: process.env.REDIS_PORT || 6379,
+  password: process.env.REDIS_PASS, 
+  tls: process.env.REDIS_TLS === "true" ? {} : undefined, // Enable TLS if needed
+});
+
+redis.on("connect", () => {
+  console.log("Connected to Redis successfully!");
+});
+
+redis.on("error", (err) => {
+  console.error("Redis connection error:", err);
+});
 
 const pool = new Pool({
   user: 'neondb_owner',
@@ -59,6 +73,8 @@ const pool = new Pool({
     rejectUnauthorized: false, 
   },
 });
+
+
 
 
 
